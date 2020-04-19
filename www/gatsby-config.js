8357590b36644a89c4da2917fcb8e6f8193845c7
Aniket Handa <atneik@gmail.com>
2020-04-18 19:36:47 -0700

    Remove contentful

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,114 +1,57 @@
-require("dotenv").config();
-
-const siteMetadata = {
-  title: `Novela by Narative`,
-  name: `Narative`,
-  siteUrl: `https://novela.narative.co`,
-  description: `This is my description that will be used in the meta tags and important for search results`,
-  hero: {
-    heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-    maxWidth: 652,
+module.exports = {
+  siteMetadata: {
+    title: `Aniket Handa`,
+    name: `Aniket Handa`,
+    siteUrl: `https://anikethanda.com`,
+    description: `Personal website of Aniket Handa. User Experience Engineer and Prototyper`,
+    hero: {
+      heading: `Hello!`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/atneik`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/atneik`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/_aniket`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/atneik/`,
+      }
+    ],
   },
-  social: [
+  plugins: [
     {
-      url: `https://twitter.com/narative`,
-    },
-    {
-      url: `https://behance.com/narative`,
-    },
-    {
-      url: `https://github.com/narative`,
-    },
-    {
-      url: `https://instagram.com/narative.co`,
-    },
-    {
-      url: `https://www.linkedin.com/company/narative/`,
-    },
-    {
-      url: `https://dribbble.com/narativestudio`,
-    },
-    {
-      url: `https://youtube.com`,
-    },
-    {
-      name: 'stackoverflow',
-      url: `https://bit.ly/1x0885j`,
-    },
-    {
-      url: `https://digitalocean.com`,
-    },
-    {
-      url: `https://tripadvisor.com`,
-    },
-  ],
-};
-
-const plugins = [
-  {
-    resolve: "@narative/gatsby-theme-novela",
-    options: {
-      contentPosts: "content/posts",
-      contentAuthors: "content/authors",
-      rootPath: "/",
-      basePath: "/",
-      authorsPage: true,
-      mailchimp: true,
-      sources: {
-        local: true,
-        contentful: false,
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
       },
     },
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Novela by Narative`,
-      short_name: `Novela`,
-      start_url: `/`,
-      background_color: `#fff`,
-      theme_color: `#fff`,
-      display: `standalone`,
-      icon: `src/assets/favicon.png`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Aniket Handa`,
+        short_name: `Aniket`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
     },
-  },
-  {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "UA-118232427-3",
-    },
-  },
-  {
-    resolve: "gatsby-plugin-mailchimp",
-    options: {
-      endpoint:
-        "https://narative.us19.list-manage.com/subscribe/post?u=65ef169332a03669b9538f6ef&amp;id=c55c426282",
-    },
-  },
-];
-
-/**
- * For development purposes if there's no Contentful Space ID and Access Token
- * set we don't want to add in gatsby-source-contentful because it will throw
- * an error.
- *
- * To enanble Contentful you must
- * 1. Create a new Space on contentful.com
- * 2. Import the Contentful Model from @narative/gatsby-theme-novela/conteful
- * 3. Add .env to www/ (see www/env.example)
- * 4. Enable contentful as a source in this file for @narative/gatsby-theme-novela
- */
-if (process.env.CONTENTFUL_SPACE_ID && process.env.CONTENTFUL_ACCESS_TOKEN) {
-  plugins.push({
-    resolve: "gatsby-source-contentful",
-    options: {
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    },
-  });
-}
-
-module.exports = {
-  siteMetadata,
-  plugins,
+  ],
 };
